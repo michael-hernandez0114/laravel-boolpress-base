ede41aa50454cb9441a1721c9f98b27bbaa943bd
Michael Hernandez <gammabeta010@gmail.com>
2020-05-19 21:11:55 +0200

    WIP DB table

---
+++ database/migrations/2020_05_19_190551_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->varchar('title');
+            $table->varchar('author', 100);
+            $table->text('message');
+            $table->text('profile_icon');
+            $table->varchar('category', 20);
+            $table->integer('post_position');
+            $table->boolean('published');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
